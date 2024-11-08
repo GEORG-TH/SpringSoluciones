9e53ac6afee232ca76e2f44483a2f550c2187fe8
Georg Thierry <georg.delacruzacho@gmail.com>
2024-11-08 17:30:22 -0500

    Primer Commit

---
+++ EstudiantesCasoII/src/main/java/com/example/EstudiantesCasoII/EstudiantesCasoIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EstudiantesCasoII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesCasoIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesCasoIiApplication.class, args);
+	}
+
+}
---
+++ EstudiantesCasoII/src/test/java/com/example/EstudiantesCasoII/EstudiantesCasoIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EstudiantesCasoII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesCasoIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
